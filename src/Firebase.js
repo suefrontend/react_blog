ed02a776d69350a6c8d1dc14e3b3d54e12cf7a16
Kaya0312 <oyama_kayano@yahoo.co.jp>
2019-11-19 23:27:14 +0900

    Get back firebase.js

--- src/Firebase.js
+++ src/Firebase.js
@@ -1,18 +1,16 @@
-import * as firebase from 'firebase';
-import firestore from 'firebase/firestore'
+import firebase from 'firebase';
 
-const settings = {timestampsInSnapshots: true};
+const firebaseApp = firebase.initializeApp({
+  // copy and paste your firebase credential here
+  apiKey: "AIzaSyCFV8Ilr04HXl7kNU0SDhMIIX5teJBTeQM",
+  authDomain: "react-blog-980ea.firebaseapp.com",
+  databaseURL: "https://react-blog-980ea.firebaseio.com",
+  projectId: "react-blog-980ea",
+  storageBucket: "react-blog-980ea.appspot.com",
+  messagingSenderId: "9690312276",
+  appId: "1:9690312276:web:03a71bb41041186f29c7c1"
+});
 
-const config = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_AUTH_DOMAIN",
-  databaseURL: "YOUR_DATABASE_URL",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_STORAGE_BUCKET",
-  messagingSenderId: "YOUR_MESSAGING_ID"
-};
-firebase.initializeApp(config);
+const db = firebaseApp.firestore();
 
-firebase.firestore().settings(settings);
-
-export default firebase;
+export {db};
