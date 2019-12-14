76d0da9c783a75db8f4a6872182cfcebc4180c39
Kaya0312 <oyama_kayano@yahoo.co.jp>
2019-12-14 16:41:09 +0900

    Set props

--- src/components/App.js
+++ src/components/App.js
@@ -1,26 +1,46 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Header from './Header';
-import Home from './Home';
-import Edit from './Edit';
-import Show from './Show';
-import Create from './Create';
-import EditPost from './EditPost';
+import Home from '../pages/Home';
+import Edit from '../pages/Edit';
+import Show from '../pages/Show';
+import Create from '../pages/Create';
+import EditPost from '../pages/EditPost';
 import './Main.css';
 import { Router, Route, Switch } from 'react-router-dom';
 import history from '../history';
+import { db } from "../firebase";
+import { getThemeProps } from '@material-ui/styles';
 
 function App() {
+    
+    const [articles, setArticles] = useState([]);
+
+    useEffect(() => {
+
+        return db.collection('articles').onSnapshot(query => {
+            setArticles( query.docs.map(m => m.data()) );
+          });        
+        
+    }, []);
+
+    console.log("TESET", articles);
+
     return (
+        <div>
+            {/* {articles.map(m => <li>{m.title}</li>)} */}
     <Router history={history}>
         <Header />
         <Switch>
-            <Route exact path='/' component={Home} />
+            {/* <Route exact path='/' component={Home} articles={articles} /> */}
+            <Route exact path='/' render={(routeProps) => (<Home {...routeProps} articles={articles}/>)}  />
+
             <Route exact path='/create' component={Create} />
             <Route exact path='/edit' component={Edit} />
             <Route exact path='/editPost/:id' component={EditPost} />
-            <Route exact path='/show/:id' component={Show} />
+            <Route exact path='/show/:id' component={Show}  />
         </Switch>
     </Router>
+    </div>
     );
 }
 
